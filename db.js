1911fdd64d72448a5e5f6771415e069ca8408448
OliverMcMillen <196836907+OliverMcMillen@users.noreply.github.com>
2025-06-16 23:13:39 -0400

    Updates

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
   user: '436_mysql_user',
   password: '123pwd456',
   //need to change to '436db'
-  database: 'assignment4db',
+  database: '436db',
 });
 
 module.exports = db;
