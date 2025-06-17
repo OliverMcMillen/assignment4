b0f0b57f036c2bce538a72ff2d07542d0113f2cf
OliverMcMillen <196836907+OliverMcMillen@users.noreply.github.com>
2025-06-16 22:26:51 -0400

    Adjustments to db.js to use mandatory db configuration credentials

--- db.js
+++ db.js
@@ -2,8 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
+  user: '436_mysql_user',
+  password: '123pwd456',
+  //need to change to '436db'
   database: 'assignment4db',
 });
 
